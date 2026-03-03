909c5149d21954c3e81b0b083b7b02d34f25dbd5
cognitiveking2007 <ratakondalalith@gmail.com>
2026-03-03 11:40:06 +0530

    Add files via upload

---
+++ ATP/week1/2-big_number_in_given_three_numbers.js
@@ -0,0 +1,13 @@
+//big number in given three numbers
+let a=1;
+let b=30;
+let c=90;
+if(a>b&&a>c){
+    console.log("a is big number")
+}
+else if(b>c){
+    console.log("b is the big number");
+}
+else{
+    console.log("c is the big number");
+}
