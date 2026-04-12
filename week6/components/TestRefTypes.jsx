6f4db699e1e15aafff9883a170da4cd930c34f24
cognitiveking2007 <ratakondalalith@gmail.com>
2026-04-12 16:56:07 +0530

    Add files via upload

---
+++ week6/components/TestRefTypes.jsx
@@ -0,0 +1,24 @@
+/*import { useState } from "react";
+
+function TestRefTypes{
+const [user,SetUser]=useState({username:"manish",age:99,city:"baroda"})
+const [marks,setMarks]=useState([10,20,30]);
+
+//update user state
+const updateUser=() => {
+    SetUser({ ...user,username:"sai",age:19});
+}
+//update marks
+const updateMarks=()=>{
+    setMarks([40, ...marks]);
+};
+
+return(
+    <div>
+        <p>Username:{user.username}</p>
+        <p>Username:{user.age}</p>
+        <p>Username:{user.username}</p>
+
+    </div>
+)
+}*/
